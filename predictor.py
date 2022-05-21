40ac2506f366cf1f6e07f00a2657c47218dc4479
Daniil Konstantinov <dkonstantinov0@gmail.com>
2022-05-21 15:29:44 +0300

    add new files

---
+++ predictor.py
@@ -0,0 +1,101 @@
+import random
+import re
+from collections import defaultdict
+from typing import List
+
+
+class FindAnswer:
+    def __init__(self, n_letters=5) -> None:
+        self._number_of_letters = n_letters
+        self._all_letters = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+
+        self._current_regular_expression = [self._all_letters] * self._number_of_letters
+        self._known_letters = defaultdict(
+            int
+        )  # буквы которые точно должны быть в слове
+        self._all_words = []
+        self._current_available_words = []  # current pool of words
+
+        self._previous_words_states = []  # полученные состояния по словам
+        self._previous_words = []  # введенные слова
+        self._load_words()
+
+    def reset(self):
+        self._previous_words.clear()
+        self._previous_words_states.clear()
+        self._known_letters.clear()
+        self._current_available_words = self._all_words.copy()
+        self._current_regular_expression = [self._all_letters] * self._number_of_letters
+
+    def get_random_word(self):
+        return random.choice(self._all_words)
+
+    def _preprocessing_text(self, lines: List[str]):
+        new_lines = [word.strip() for word in lines]
+        new_lines = [word.lower() for word in new_lines if word.isalpha()]
+        new_lines = [word for word in new_lines if len(word) == self._number_of_letters]
+        new_lines = [word for word in new_lines if "ё" not in word]
+        return new_lines
+
+    def _load_words(self, filename="rus_words_no_duplicates.txt"):
+        with open(filename, "r") as f:
+            new_lines = f.readlines()
+        # new_lines = [word.decode("WINDOWS-1251") for word in lines]
+        self._all_words = self._preprocessing_text(new_lines)
+        self._current_available_words = self._all_words.copy()
+
+    def _apply_regular(self):
+        pattern = f"[{']['.join(self._current_regular_expression)}]"
+        prog = re.compile(pattern)
+        regular_list = [
+            word for word in self._current_available_words if prog.search(word)
+        ]
+        RESULT_LIST = []
+        for word in regular_list:
+            flag = True
+            for letter in self._known_letters:
+                if not letter in word:
+                    flag = False
+            if flag:
+                RESULT_LIST.append(word)
+        self._current_available_words = RESULT_LIST
+
+    def _remove_letter_from_regular(self, letter, position=None):
+        if not letter in self._known_letters:
+            for i in range(self._number_of_letters):
+                self._current_regular_expression[i] = self._current_regular_expression[
+                    i
+                ].replace(letter, "")
+        else:
+            self._current_regular_expression[
+                position
+            ] = self._current_regular_expression[position].replace(letter, "")
+
+    def get_next_word(self):
+        if len(self._current_available_words) == 0:
+            raise Exception("No words more")
+        return random.choice(self._current_available_words)
+
+    def response_from_game(self, word=None, state_of_word=None):
+        assert len(word) == len(state_of_word) == self._number_of_letters
+        # - : now such letter
+        # * : letter not in its position
+        # + : letter in its position
+
+        self._previous_words.append(word)
+        self._previous_words_states.append(state_of_word)
+        for i, letter, state_of_letter in zip(range(len(word)), word, state_of_word):
+            if state_of_letter == "-":
+                self._remove_letter_from_regular(letter, i)
+            elif state_of_letter == "+":
+                self._known_letters[letter] += 1
+                self._current_regular_expression[i] = letter
+            elif state_of_letter == "*":
+                self._known_letters[letter] += 1
+                self._current_regular_expression[i] = self._current_regular_expression[
+                    i
+                ].replace(letter, "")
+            else:
+                raise Exception("Wrong state")
+
+        self._apply_regular()
